3617aa207c5a53c0588f09dc80059a7ca198a417
Süleyman Koparır <suleymankoparir@gmail.com>
2022-07-22 21:45:29 +0300

    CreditCard

---
+++ HW/CreditCard/CreditCard/CreditCard.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    internal class CreditCard
+    {
+        private string holderName;
+        private string number;
+        private string expireDate;
+        private string ccv;
+        public string HolderName {
+            get
+            {
+                return holderName;
+            }
+            set
+            {
+                holderName = value;
+            }
+        }
+
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                Regex regex = new Regex("^[0-9]+$");
+                if (value.Length != 16)
+                    throw new CreditCardException("Invalid Number length. Number must contain 16 digits");
+                if(!regex.IsMatch(value))
+                    throw new CreditCardException("Invalid Number length. Number must contain digits");
+                number = value;
+            }
+        }
+        public string ExpireDate
+        {
+            get
+            {
+                return expireDate;
+            }
+            set
+            {
+                if(value.Length!=5)
+                    throw new CreditCardException("Invalid ExpireDate format");
+                if (value[2]!='/')
+                    throw new CreditCardException("Invalid ExpireDate format");
+                int month;
+                int year;
+                try
+                {
+                   month =int.Parse(value.Substring(0, 2));
+                   year = 2000+int.Parse(value.Substring(3, 2));
+                }
+                catch (Exception e)
+                {
+                    throw new CreditCardException("Invalid ExpireDate format. "+e.ToString());
+                }
+                if(!(month>=1&&month<=12))
+                    throw new CreditCardException("Invalid month value. The Month value must be between 1 and 12");
+                if (year > DateTime.Today.Year)
+                    expireDate = value;
+                else if(year == DateTime.Today.Year)
+                {
+                    if(month>=DateTime.Today.Month)
+                        expireDate = value;
+                    else
+                        throw new CreditCardException("Invalid month value. Since the year values are equal, the month value must be greater than or equal to "+DateTime.Today.Month);
+                }
+                else
+                    throw new CreditCardException("Invalid year value. Year value must be greater than or equal to "+DateTime.Today.Year);
+            }
+        }
+        public string Ccv
+        {
+            get
+            {
+                return ccv;
+            }
+            set
+            {
+                Regex regex = new Regex("^[0-9]+$");
+                if (value.Length != 3 || !regex.IsMatch(value))
+                    throw new CreditCardException("Invalid Ccv format");
+                ccv = value;
+            }
+        }
+
+        public string ToString()
+        {
+            return "Number :"+number+" Holder : "+holderName+" Ccv : "+ccv+" Expire Date : "+expireDate;
+        }
+    }
+}
---
+++ HW/CreditCard/CreditCard/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard creditCard = new CreditCard();
+
+            creditCard.Number = "1234567812345678";
+            //creditCard.Number = "12345678123456781";  //throws exception
+            //creditCard.Number = "AVC456781234567A";   //throws exception
+
+            creditCard.ExpireDate = "12/24";
+            //creditCard.ExpireDate = "12/123";     //throws exception
+            //creditCard.ExpireDate = "12,24";      //throws exception
+            //creditCard.ExpireDate = "ab/cd";      //throws exception
+            //creditCard.ExpireDate = "12/20";      //throws exception
+            //creditCard.ExpireDate = "13/24";      //throws exception
+            //creditCard.ExpireDate = "00/24";      //throws exception
+            //creditCard.ExpireDate = "06/22";      //throws exception
+
+            creditCard.HolderName = "Süleyman Koparır";
+
+            creditCard.Ccv = "927";
+            //creditCard.Ccv = "9271";  //throws exception
+            //creditCard.Ccv = "A12";   //throws exception
+
+            Console.WriteLine(creditCard.ToString());
+            //Number :1234567812345678 Holder : Süleyman Koparır Ccv : 927 Expire Date : 12/24
+
+            Console.ReadKey();
+        }
+    }
+}

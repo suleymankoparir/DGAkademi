85b0f336fed0ef32578440dd047fef23bef165de
Süleyman Koparır <suleymankoparir@gmail.com>
2022-08-06 14:58:23 +0300

    Heroku migration fix

--- W-01/W-01/Program.cs
+++ W-01/W-01/Program.cs
@@ -60,11 +60,6 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 //builder.Services.AddMvc();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
